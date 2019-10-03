6a49255230caf06ee908346ddeb70f697b145ad1
renosirait <50711351+renosirait@users.noreply.github.com>
2019-10-03 22:15:38 +0700

    Update toplistico.js

--- toplistico.js
+++ toplistico.js
@@ -9,4 +9,10 @@ var pic = document.getElementById("pic");
 pic.style.width=slideAmount+"px";
 pic.style.height=slideAmount+"auto";
 }
-df
+
+$('[data-countdown]').each(function() {
+  var $this = $(this), finalDate = $(this).data('countdown');
+  $this.countdown(finalDate, function(event) {
+    $this.html(event.strftime('%Dd %Hh %Mm'));
+  });
+});
