9786187fc09827e1a53b21317b4d0d97656be971
renosirait <50711351+renosirait@users.noreply.github.com>
2019-10-16 14:06:41 +0700

    Update toplistico.js

--- toplistico.js
+++ toplistico.js
@@ -10,3 +10,18 @@ pic.style.width=slideAmount+"px";
 pic.style.height=slideAmount+"auto";
 }
 
+getData('btc', 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD');
+
+getData('eth', 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD');
+
+getData('ltc', 'https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=USD');
+
+getData('bch', 'https://min-api.cryptocompare.com/data/price?fsym=BCH&tsyms=USD');
+
+function getData(prefix, url) {
+  $.getJSON(url, function(data) {
+    $.each(data, function(key, val) {
+      $('.' + prefix + '-' + key.toLowerCase()).html(val);
+    });
+  });
+}
